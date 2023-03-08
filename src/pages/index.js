20ea790124cf1927fe3e786562781d61f50bfba7
unknown <caroline.daniel@showtime.net>
2023-03-08 18:09:49 -0500

    change

--- src/pages/index.js
+++ src/pages/index.js
@@ -127,7 +127,7 @@ const IndexPage = () => {
   return (
     <main style={pageStyles}>
       <h1 style={headingStyles}>
-        Congratulations
+        Congratulations Will this ever go up?
         <br />
         <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
       </h1>
